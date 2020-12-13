e8693079b05661a2e324cbb72fbda831d25743cf
Lars Thorup <lars@zealake.com>
2020-12-13 20:18:40 +0100

    weird floats

--- test/math.test.js
+++ test/math.test.js
@@ -9,5 +9,8 @@ describe('math', function () {
     it('should work for almost-integers', function () {
       expect(Math.sqrt(15.99999999999999999)).to.equal(4);
     });
+    it('should work for weird floats', function () {
+      expect(Math.sqrt(12.3456)).to.equal(3.51);
+    })
   });
 });
